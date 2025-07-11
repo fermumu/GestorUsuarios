823d73567996938fa7b3c9f009862b9882680f40
Fernando Muñoz <fernando1986caballeria@gmail.com>
2025-07-10 21:38:37 -0500

    Creacion de archivo para inicializar la API

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 
+const express = require("express");
+const app = express();
+const sequelize = require("./config/dataBaseConfig");
+const userRoutes = require("./routes/userRoutes");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync()
+    .then(() => {
+        console.log("Base de datos encontrada");
+        app.listen(PORT, ()=>{
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+            
+        });
+    }).catch(err => {
+        console.error("No hay coneccion con la base de datos: ",  err);
+        
+    });
+
